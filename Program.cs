530b3f540d3bf73befcc175ba80a414c7fcc4949
heba <heba@AST-PF4LFV14>
2024-06-25 20:20:24 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+
+namespace Week4Practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] menuOptions = { "Pizza", "Pasta", "Salad", "Drinks" };
+
+            int choice;
+
+            // Display the menu options
+            Console.WriteLine("Welcome to our Restaurant!");
+            Console.WriteLine("Here are your options:");
+            for (int i = 0; i < menuOptions.Length; i++)
+            {
+                Console.WriteLine((i + 1) + ". " + menuOptions[i]);
+            }
+
+            // Get user input for their choice
+            Console.Write("Enter your choice (1-" + menuOptions.Length + "): ");
+            choice = Convert.ToInt32(Console.ReadLine());
+
+            // Validate user input and display chosen option
+            if (choice >= 1 && choice <= menuOptions.Length)
+            {
+                Console.WriteLine("You selected: " + menuOptions[choice - 1]);
+            }
+            else
+            {
+                Console.WriteLine("Invalid choice. Please try again.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
